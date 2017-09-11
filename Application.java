8ba4d9a0bee1c2ad2eb02d0cb56b0427c2a8479b
Iwo <iwo.tyszkowski@gmail.com>
2017-09-11 13:22:40 +0200

    Add main method that runs whole program

--- Application.java
+++ Application.java
@@ -3,5 +3,26 @@ import java.io.File;
 import java.util.Scanner;
 
 public class Application {
-    
+
+    public static void main(String... args) {
+        long startTime = System.currentTimeMillis();
+        View view = new View();
+
+        for (int i=0; i < args.length; i++) {
+            FileContent newFile = new FileContent(args[i]);
+            CharIterator newCharIterator = new CharIterator(newFile);
+            WordIterator newWordIterator = new WordIterator(newFile);
+            StatisticalAnalysis newCharAnalysis = new StatisticalAnalysis(newCharIterator);
+            StatisticalAnalysis newWordAnalysis = new StatisticalAnalysis(newWordIterator);
+
+            view.printFilename(newFile.getFilename());
+            // TODO: view.print("Char count", xxx);
+            // TODO: view.print("Word count", xxx);
+            // TODO: view.print("Dict size", xxx);
+        }
+
+        long stopTime = System.currentTimeMillis();
+        long elapsedTime = (stopTime - startTime);
+        view.printTime("Benchmark time:", elapsedTime);
+    }
 }
