e8484d7b049b904d8aa4cd512851b22c24fb3a75
Iwo <iwo.tyszkowski@gmail.com>
2017-09-11 13:26:30 +0200

    Add getList method

--- CharIterator.java
+++ CharIterator.java
@@ -13,4 +13,15 @@ public class CharIterator implements Iterator {
         index = 0;
     }
 
+    private LinkedList<E> getList() {
+        LinkedList<String> list = new LinkedList<Character>();
+        for (int i=0; i<fileContent.getWholeString().length(); i++) {
+            list.add(ileContent.getWholeString().substring(i, i+1));
+        }
+        return itemsList;
+    }
+
+    private LinkedList<E> getList() {
+        return charList;
+    }
 }
