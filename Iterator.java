765b3ee81bd3405d50dfc85130a6cae72f5cfe96
Iwo <iwo.tyszkowski@gmail.com>
2017-09-11 13:23:37 +0200

    Add Iterator class with proper methods

---
+++ Iterator.java
@@ -0,0 +1,11 @@
+
+public interface Iterator<E> {
+
+    public boolean hasNext();
+
+    public E next();
+
+    // public void remove();
+
+    // public void forEachRemaining(Iterator<? super E> action);
+}
