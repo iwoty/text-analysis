dfff9d2be22a813f36fbfe01b3db091b11bee83e
Iwo <iwo.tyszkowski@gmail.com>
2017-09-11 13:34:03 +0200

    Add method for future development TODO

--- StatisticalAnalysis.java
+++ StatisticalAnalysis.java
@@ -10,4 +10,20 @@ public class StatisticalAnalysis {
     public int countOf(Iterator<String> iterator) {
         return iterator.fileContent.getWholeString().length();
     }
+    // TODO:
+    // public int countOf(String... elems) {
+    //     return xxxxxx;
+    // }
+    //
+    // public int dictionarySize() {
+    //     return xxx;
+    // }
+    //
+    // public int size() {
+    //     return xxx;
+    // }
+    //
+    // public Set<String> occurMoreThan(Integer) {
+    //     return setXxxx;
+    // }
 }
